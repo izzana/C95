dc5ce0db67c9bbac57bb07c39ee57cf54e54e0d3
Izzana Barbosa Martins <91554353+izzana@users.noreply.github.com>
2023-03-22 14:58:56 -0300

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import Home from "../screens/Home";
+
+//navegador de aba inferior
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Feed') {//definindo ícones diferentes para as telas diferentes
+                        iconName = focused
+                            ? 'book'
+                            : 'book-outline';
+                    } else if (route.name === 'Início') {
+                        iconName = focused ? 'home' : 'home-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="Início" component={Home} />
+        </Tab.Navigator>
+    );
+}
+
+export default BottomTabNavigator
